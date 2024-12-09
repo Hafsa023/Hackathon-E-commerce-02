f655ef54999e25c077238b2dc3551daa3e878fd7
khan561hunter <waniakhan561@gmail.com>
2024-12-10 00:47:23 +0500

    fixed

--- hackathon/app/components/footer.tsx
+++ hackathon/app/components/footer.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Link from "next/link";
 
 import { IoLocationOutline } from "react-icons/io5";
--- hackathon/app/hero/page.tsx
+++ hackathon/app/hero/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Image from "next/image";
 import React from "react";
 import Img from "@/app/assets/Image.png";
