ee94573ccbbb1655984f523ccf6abedfdc5bb491
khan561hunter <waniakhan561@gmail.com>
2024-12-10 01:01:50 +0500

    again

--- hackathon/next.config.ts
+++ hackathon/next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    distDir: 'build',
+    // Add other config options here
 };
 
 export default nextConfig;
